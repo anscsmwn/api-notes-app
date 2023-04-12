13ec5fbdb6c4943631b01575835da5e364bde01a
anca <annascasmawannn@gmail.com>
2023-04-12 20:11:41 +0800

    implement plugin hapi and validation

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const Hapi = require('@hapi/hapi')
-const routes = require('./routes')
 
+const NotesService = require('./services/inMemory/NotesService')
+const notes = require('./api/notes')
+const NotesValidator = require('./validator/notes')
 const init = async () => {
+  const notesService = new NotesService()
+
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -11,7 +15,15 @@ const init = async () => {
       },
     },
   })
-  server.route(routes)
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  })
+
   await server.start()
   console.log(`Server berjalan pada ${server.info.uri}`)
 }
